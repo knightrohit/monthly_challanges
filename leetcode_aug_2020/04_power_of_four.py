dcf17dba0e064d509569215d8f0e96d7e39fdb78
Rohit <knight.rohit@gmail.com>
2020-08-05 08:00:36 -0500

    bitwise operator

--- leetcode_aug_2020/04_power_of_four.py
+++ leetcode_aug_2020/04_power_of_four.py
@@ -11,3 +11,9 @@ class Solution:
 
         val = math.log(num, 4)
         return not(val > int(val))
+
+
+# Using bitwise operator
+class Solution:
+    def isPowerOfFour(self, num: int) -> bool:
+        return num > 0 and num & (-num) == num and num & 0xaaaaaaaa == 0
