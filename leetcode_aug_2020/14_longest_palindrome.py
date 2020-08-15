193813e650525ade6b73fbba078690c49191bdcb
Rohit <knight.rohit@gmail.com>
2020-08-14 20:06:30 -0500

    using counter

---
+++ leetcode_aug_2020/14_longest_palindrome.py
@@ -0,0 +1,21 @@
+"""
+Time Complexity = O(N)
+Space Complexity = O(1)
+"""
+from collections import Counter
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+       
+        ch_dict = Counter(s)
+        out = 0
+
+        for val in ch_dict.values():
+            
+            if val % 2 == 0:
+                out += val                
+            
+            else:
+                out += val if not out % 2 else val - 1
+
+        return out
