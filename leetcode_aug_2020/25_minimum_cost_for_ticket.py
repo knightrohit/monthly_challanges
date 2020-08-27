9e56152a01fe23a79369a893f448b8cae4f021fb
Rohit <knight.rohit@gmail.com>
2020-08-26 22:49:42 -0500

    added bottom up dp approach

--- leetcode_aug_2020/25_minimum_cost_for_ticket.py
+++ leetcode_aug_2020/25_minimum_cost_for_ticket.py
@@ -1,6 +1,7 @@
 """
 Time/Space Complexity = O(last day)
 """
+# Top down DP
 from functools import lru_cache
 
 class Solution:
@@ -21,3 +22,21 @@ class Solution:
                 return min_cost(days + 1)            
         
         return min_cost()
+
+
+# Bottom Up DP
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        
+        # dp[i] minimum cost till ith date
+        dp = [0]*(days[-1] + 1)
+        setdays = set(days)
+        
+        for i in range(1, days[-1]+1):
+            if i not in setdays:
+                dp[i] = dp[max(0,i-1)]
+            else:
+                #dp[i] = min(dp[max(0,i-1)]+costs[0], dp[max(0,i-7)]+costs[1], dp[max(0,i-30)]+costs[2])
+                dp[i] = min(dp[(max(0, i-d))] + c for d, c in zip((1,7,30), costs))
+            
+        return dp[-1]
