e2748a87728d3cecb7305b4f11e0993f02cc2019
Rohit <knight.rohit@gmail.com>
2020-09-07 15:46:08 -0500

    using zip

--- leetcode_sept_2020/7_word_pattern.py
+++ leetcode_sept_2020/7_word_pattern.py
@@ -23,3 +23,29 @@ class Solution:
                 return False
 
         return True
+
+
+# Second Approach using zip 
+class Solution:
+    def wordPattern(self, pattern: str, str: str) -> bool:
+        
+        if not (pattern and str) or len(pattern) != len(str.split()):
+            return False
+        
+        word_dict = dict()
+        str_list = str.split()
+        added = set()
+
+        for p, s in zip(pattern, str_list):
+            
+            val = word_dict.get(p)
+
+            if not val and s not in added:
+                word_dict[p] = s
+                added.add(s)
+                continue
+            
+            if val != s:
+                return False
+
+        return True
