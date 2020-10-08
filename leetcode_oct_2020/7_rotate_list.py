13d485f1413e10437f3a8e66796ded5d5ff05c19
Rohit <knight.rohit@gmail.com>
2020-10-08 00:41:26 -0500

    added constant space

--- leetcode_oct_2020/7_rotate_list.py
+++ leetcode_oct_2020/7_rotate_list.py
@@ -33,3 +33,37 @@ class Solution:
             node = node.next
             
         return head.next
+
+
+"""
+Time Complexit = O(N)
+Space Complexity = O(1)
+"""
+
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        
+        if not head:
+            return head
+        
+        if k == 0:
+            return head
+
+        node = head
+        size = 0
+        while node.next:
+            node = node.next
+            size += 1
+        node.next = head
+        size += 1
+
+        k %= size 
+        
+        node = head
+        for _ in range(size - (k + 1)):
+            node = node.next
+        
+        head = node.next
+        node.next = None
+
+        return head
