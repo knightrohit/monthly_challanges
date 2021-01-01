f963cc12dbc1e33f0bb458dd1a1b6ef11bcdc965
Rohit <knight.rohit@gmail.com>
2021-01-01 09:04:24 -0600

    using counter

---
+++ leetcode_jan_2021/1_palindrome_permutation.py
@@ -0,0 +1,24 @@
+"""
+Time/Space Complexity - O(N)
+"""
+from collections import Counter
+
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        
+        if not s:
+            return False
+        
+        count = Counter(s)
+        
+        odd = 0
+        for val in count.values():
+            if val%2:
+                odd += 1
+            if odd > 1:
+                return False
+            
+        if odd:
+            return True if len(s)%2 else False
+        else:
+            return True if not len(s)%2 else False
