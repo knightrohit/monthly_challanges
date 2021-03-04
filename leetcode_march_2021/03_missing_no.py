351c062889980d5c9de2bc73d7d4beab6e797078
Rohit <knight.rohit@gmail.com>
2021-03-03 19:33:00 -0600

    using xor

--- leetcode_march_2021/03_missing_no.py
+++ leetcode_march_2021/03_missing_no.py
@@ -4,3 +4,20 @@ Time/Space complexity = O(N)
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:        
         return next(iter(set(range(len(nums)+1)) - set(nums)))
+
+
+# Method - 2
+"""
+Time Complexity = O(N)
+Space Complexity = O(1)
+"""
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        missing = len(nums)
+        
+        for i, val in enumerate(nums):
+            missing ^= (i ^ val)
+            
+        return missing
